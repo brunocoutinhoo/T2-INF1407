c516952fdf916df9ea26f1de57bf39b9b7e39ed2
brunocoutinhoo <brunocoutinho64@gmail.com>
2021-11-18 02:10:30 -0300

    primeiro esboço models

--- projeto/models.py
+++ projeto/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Produto(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    nome = models.CharField(max_length=50)
+    unidade = models.CharField(max_length=10)
+    descricao = models.CharField(max_length=100, blank=True, null=True)
