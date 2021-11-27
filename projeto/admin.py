aa348a0a5c928163e10e3dc9ad82a3a76ded2c72
brunocoutinhoo <brunocoutinho64@gmail.com>
2021-11-27 14:24:24 -0300

    adicionando paginas ao site admin

--- projeto/admin.py
+++ projeto/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Feira, Barraca, Produto, ListaCompras, ProdutoListaCompras
 
 # Register your models here.
+
+admin.site.register(Feira)
+admin.site.register(Barraca)
+admin.site.register(Produto)
+admin.site.register(ListaCompras)
+admin.site.register(ProdutoListaCompras)
