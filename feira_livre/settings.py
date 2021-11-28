34f9d472d3a4285bfcb764087dc9809d86c9fa74
Pipeshokan <43938493+Pipeshokan@users.noreply.github.com>
2021-11-28 18:19:56 -0300

    static root

--- feira_livre/settings.py
+++ feira_livre/settings.py
@@ -137,6 +137,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+if ENVIRONMENT == 'production':
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
