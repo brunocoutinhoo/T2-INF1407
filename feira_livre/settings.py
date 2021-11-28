cfc8fc11252eabb1330ef95941b1e65cc85a89c8
Pipeshokan <43938493+Pipeshokan@users.noreply.github.com>
2021-11-28 19:03:05 -0300

    bugfix django_heroku.settings

--- feira_livre/settings.py
+++ feira_livre/settings.py
@@ -165,5 +165,5 @@ if ENVIRONMENT == 'production':
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
 
-django_heroku.settings(locals)
+django_heroku.settings(locals())
 
