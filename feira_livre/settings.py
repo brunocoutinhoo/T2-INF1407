977856039204e338d0501df17284c333c1c38870
Pipeshokan <43938493+Pipeshokan@users.noreply.github.com>
2021-11-28 18:35:33 -0300

    django_heroku

--- feira_livre/settings.py
+++ feira_livre/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -156,3 +157,5 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'horadafeirasite@gmail.com'
 EMAIL_HOST_PASSWORD = os.environ.get("HORADAFEIRA_SENHA_EMAIL","")
+
+django_heroku.settings(locals)
